902d1a3310936086e45cf5c2b4ce19abe157c3a0
Gregory Eakin <greg@eakin.dev>
2023-02-25 21:11:56 -0800

    Remove the Asset junk

--- BioSimWeb/Program.cs
+++ BioSimWeb/Program.cs
@@ -5,17 +5,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddSingleton<IAssetsResolver, AssetsResolver>();
-builder.Services.AddSingleton<IAssetLoaderFactory>(ctx =>
-{
-    var factory = new AssetLoaderFactory();
 
-    // factory.Register(ctx.GetRequiredService<IAssetLoader<Sprite>>());
-    // factory.Register(ctx.GetRequiredService<IAssetLoader<SpriteSheet>>());
-
-    return factory;
-});
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
